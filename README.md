734cb5c554d72643aba65067a2a1c3bba858f310
Adam Jiwa <leader.alan.pro@live.com>
2026-04-04 13:49:58 +0800

    Added Logging Switched to udev running systemd task as running from udev gets pruned after a couple of minutes

--- README.md
+++ README.md
@@ -2,9 +2,8 @@ Script for turning off ps5 controller when Idle
 
 this ignores the gyro as I don't use it
 
-acounts for stick drift by 8 in either direction of 127.5, highest I've seen on
-my controller is 132
-
+acounts for stick drift by 16 in either direction of 127.5, highest I've seen is
+on my old DS4 controller with 143 
 
 setup
 
@@ -19,12 +18,28 @@ for ps4 DualShock
 chmod +x controllerIdle.py
 ```
 
+create a systemd task e.g. /etc/systemd/system/controller-idle.service
+```
+[Unit]
+Description=Idle shut off for play station controllers
+
+[Service]
+Type=oneshot
+RemainAfterExt=true
+ExecStart=controllerIdle.py
+User=
+Group=
+```
+
+fill out the path to controllerIdle.py and add the user/group you want to run it as
+
+
 add the udev rule replaceing <location> with this projects location e.g. /etc/udev/rules.d/90-myrules.d
 ```
 # ps5 DualSense
-ACTION=="add", SUBSYSTEMS=="input", ATTRS{name}=="DualSense Wireless Controller", RUN+="<location>/idle_controller/controllerIdle.py"
+ACTION=="add", SUBSYSTEMS=="input", ATTRS{name}=="DualSense Wireless Controller", TAG+="systemd", ENV{SYSTEMD_WANTS}="controller-idle.service" 
 # ps4 DualShock
-ACTION=="add", SUBSYSTEMS=="input", ATTRS{name}=="Wireless Controller", RUN+="<location>/idle_controller/controllerIdle.py"
+ACTION=="add", SUBSYSTEMS=="input", ATTRS{name}=="Wireless Controller", TAG+="systemd", ENV{SYSTEMD_WANTS}="controller-idle.service"
 ```
 
 reload udev rules
--- controllerIdle.py
+++ controllerIdle.py
@@ -4,6 +4,15 @@ import time
 import datetime
 import subprocess
 from select import select
+import logging
+
+
+logging.basicConfig(
+    filename = '/tmp/controllerIdle.log',
+    filemode = 'a',
+    format = '%(asctime)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
 
 # TODO handle multiple controllers
 # TODO update udev rules to use vendor and product codes
@@ -16,15 +25,19 @@ controllers = {}
 
 for device in devices:
     if (device.name in valid_controllers and device.info.vendor == vendor):
-        # print("Found valid device ", device.path, device.name, device.phys)
+        logging.info(f"Found valid device {device.name}")
         controllers[device.fd] = device
+
+if (len(controllers) < 1):
+    logging.error("Failed to find controller")
+    exit()
 
 last_valid_input = datetime.datetime.now()
 
 def validInput(event):
     global last_valid_input
     last_valid_input = datetime.datetime.now()
-    # print(f"{last_valid_input} Detected valid input {event}")
+    # logging.info(f"{last_valid_input} Detected valid input {event}")
 
 def process_event(event, device):
     match event.type:
@@ -33,21 +46,21 @@ def process_event(event, device):
         case 3:
             if (device.name == "DualSense Wireless Controller" or device.name == "Wirless Controller"):
                 if (event.code >= 0 and event.code <= 5):
-                    if (abs(127.5 - event.value) > 8):
+                    if (abs(127.5 - event.value) > 16):
                         validInput(event)
                 else:
                     validInput(event)
 
 while  True:
     endTime = last_valid_input + datetime.timedelta(minutes=3)
-    # print(f"Time Now {datetime.datetime.now()} Time to shutoff {endTime}")
+    # logging.info(f"Time Now {datetime.datetime.now()} Time to shutoff {endTime}")
     if (datetime.datetime.now() >= endTime):
+        logging.info("idle time exceeded")
         break
     r, w, x = select(controllers, [], [], 1)
     for fd in r:
         for event in controllers[fd].read():
             process_event(event, controllers[fd])
 
-# print(f"{datetime.datetime.now()} Idle time exceeded quiting")
 for controller in controllers:
-    print(subprocess.run(["bluetoothctl", "disconnect", controllers[controller].uniq]))
+    logging.info(subprocess.run(["bluetoothctl", "disconnect", controllers[controller].uniq]))
